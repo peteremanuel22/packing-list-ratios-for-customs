17ac09bf161b77dee4e80c8225d2ea3594955e1d
peteremanuel22 <97759858+peteremanuel22@users.noreply.github.com>
2025-12-18 11:18:36 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -382,3 +382,4 @@ footer_html = """
 <div class="app-footer">✨ تم التنفيذ بواسطة م / بيتر عمانوئيل – جميع الحقوق محفوظة © 2025 ✨</div>
 """
 st.markdown(footer_css, unsafe_allow_html=True)
+st.markdown(footer_html, unsafe_allow_html=True)
